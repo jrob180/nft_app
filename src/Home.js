1940a5b3ed0a0cd3103ae152b49b5b28d5a3f5ba
Jackson Roberts <jrob180@yahoo.com>
2021-09-21 01:09:50 -0400

    landing page changes

--- src/Home.js
+++ src/Home.js
@@ -5,96 +5,139 @@ function Home(){
     return(
     <div className="App">
         
-        <div class="relative bg-white overflow-hidden">
-            <div class="max-w-7xl mx-auto">
-            <div class="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
-                <svg class="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2" fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
-                <polygon points="50,0 100,0 50,100 0,100" />
-                </svg>
-
-                <div>
-                <div class="relative pt-6 px-4 sm:px-6 lg:px-8">
-                    <nav class="relative flex items-center justify-between sm:h-10 lg:justify-start" aria-label="Global">
-                    <div class="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
-                        <div class="flex items-center justify-between w-full md:w-auto">
-                        <a href="#">
-                            <span class="sr-only">Workflow</span>
-                            <img class="h-8 w-auto sm:h-10" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"/>
-                        </a>
-                        <div class="-mr-2 flex items-center md:hidden">
-                            <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false">
-                            <span class="sr-only">Open main menu</span>
-
-                            <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                            </svg>
-                            </button>
-                        </div>
-                        </div>
-                    </div>
-                    <div class="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
-                        <BrowserRouter>
-                        <Link to="/About" class="font-mono font-medium text-gray-500 hover:text-gray-900">How It Works</Link>
-                        </BrowserRouter>
-                        <a href="#" class="font-mono font-medium text-gray-500 hover:text-gray-900">Mint</a>
-                    </div>
-                    </nav>
-                </div>
-
-                <div class="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
-                    <div class="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
-                    <div class="px-5 pt-4 flex items-center justify-between">
-                        <div>
-                        <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt=""/>
-                        </div>
-                        <div class="-mr-2">
-                        <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                            <span class="sr-only">Close main menu</span>
-
-                            <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                            </svg>
-                        </button>
-                        </div>
-                    </div>
-                    <div class="px-2 pt-2 pb-3 space-y-1">
-                        <a href="#" class="font-mono block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">How It Works</a>
-
-                        <a href="#" class="font-mono block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Mint</a>
-                    </div>
-                    </div>
-                </div>
-                </div>
-
-                <main class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
-                <div class="sm:text-center lg:text-left">
-                    <h1 class="text-4xl tracking-tight font-extrabold sm:text-5xl md:text-6xl">
-                    <span class="font-mono block text-yellow-600 xl:inline">Who will hold the golden leader?</span>
-                    <span class="font-mono block text-black-600 sm:block text-4xl pt-4">The race begins October 15th</span>
+        <div class="relative flex flex-col bg-black py-16 lg:pt-0 lg:flex-col lg:pb-0">
+            <div class="flex flex-col items-start w-full max-w-xl px-4 mx-auto lg:px-8 lg:max-w-screen-xl">
+                <div class="mb-16 lg:my-40 lg:max-w-lg lg:pr-5">
+                <div class="max-w-xl mb-6">
+                    <div>
+                    <h1 class="inline-block px-3 py-px mb-10 text-3xl font-bold font-sans tracking-wider text-gray-300 rounded-full bg-teal-gray-300">
+                        Elemental
                     </h1>
-                    <p class="font-mono mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                    Collect one of each of the five classes of men to mint the golden leader and take home the entire pot!
-                    </p>
-                    <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-                    <div class="rounded-md shadow">
-                        <a href="#" class="font-mono w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-yellow-600 hover:bg-yellow-700 md:py-4 md:text-lg md:px-10">
-                        Connect Metamask
-                        </a>
                     </div>
-                    <div class="mt-3 sm:mt-0 sm:ml-3">
-                        <BrowserRouter>
-                            <Link to="/About" class="font-mono w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-gray-700 bg-yellow-100 hover:bg-yellow-200 md:py-4 md:text-lg md:px-10">How It Works</Link>
-                        </BrowserRouter>
+
+                    <h2 class="max-w-lg mb-6 font-sans text-xl font-bold tracking-tight text-gray-400 sm:text-lg sm:leading-none">
+                    [The race begins October 25th]<br class="hidden md:block" />
+
+                    </h2>
+                    <p class="text-base font-sans text-gray-600 md:text-lg mb-10 mt-10">
+                    243 years ago, the elements were scattered across the universe. It is up to you to harness the power of all the elements and craft the almighty!
+                    </p>
+                </div>
+                <div class="flex flex-col items-center content-center justify-around md:flex-row">
+                    <a
+                    href="/"
+                    class="inline-flex items-center content-center justify-center w-full h-12 px-6 mb-3 font-sans tracking-wide text-white"
+                    >
+                    [Connect Metamask]
+                    </a>
+                    <a
+                    href="/"
+                    class="inline-flex items-center justify-center w-full h-12 px-6 mb-3 font-sans tracking-wide text-white"
+                    >
+                    [Learn More]
+                    </a>
+
+                </div>
+                </div>
+            </div>
+            <div class="inset-y-0 right-0 w-full max-w-xl px-4 mx-auto lg:pl-8 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-1/2 lg:max-w-full lg:absolute xl:px-0">
+                <img
+                class="object-cover w-full h-56 rounded shadow-lg lg:rounded-none lg:shadow-none sm:h-96 lg:h-full"
+                src="https://cdna.artstation.com/p/marketplace/presentation_assets/000/968/606/large/file.jpg?1622646495"
+                alt=""
+                />
+            </div>
+            </div>
+
+
+
+            <div class="bg-black px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+                <div class="bg-black max-w-xl mb-20 md:mx-auto sm:text-center lg:max-w-2xl md:mt-15">
+
+                    <h2 class="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-300 sm:text-4xl md:mx-auto">
+                        Roadmap
+                    </h2>
+                    <p class="text-base text-gray-600 md:text-lg">
+                    Our future, outlined in a few steps.
+                    </p>
+                </div>
+                <div class="grid gap-4 row-gap-5 sm:grid-cols-2 lg:grid-cols-4">
+                    <div class="flex flex-col justify-center p-5 border rounded shadow-sm">
+                    <div class="content-center items-center">
+                        <h6 class="text-white mb-2 font-sans leading-5">Phase I</h6>
+                        <p class="font-sans mb-3 text-sm text-gray-500">
+                        Grow our community on discord. Engage with our community. Develop to fit their needs.
+                        </p>
+                    </div>
+                    </div>
+
+                    <div class="flex flex-col justify-center p-5 border rounded shadow-sm">
+                    <div class="content-center items-center">
+                        <h6 class="text-white mb-2 font-sans leading-5">Phase II</h6>
+                        <p class="font-sans mb-3 text-sm text-gray-500">
+                        Grow our community on discord. Engage with our community. Develop to fit their needs.
+                        </p>
+                    </div>
+                    </div>
+
+                    <div class="flex flex-col justify-center p-5 border rounded shadow-sm">
+                    <div class="content-center items-center">
+                        <h6 class="text-white mb-2 font-sans leading-5">Phase III</h6>
+                        <p class="font-sans mb-3 text-sm text-gray-500">
+                        Grow our community on discord. Engage with our community. Develop to fit their needs.
+                        </p>
+                    </div>
+                    </div>
+
+                    <div class="flex flex-col justify-center p-5 border rounded shadow-sm">
+                    <div class="content-center items-center">
+                        <h6 class="text-white mb-2 font-sans leading-5">Phase IV</h6>
+                        <p class="font-sans mb-3 text-sm text-gray-500">
+                        Grow our community on discord. Engage with our community. Develop to fit their needs.
+                        </p>
                     </div>
                     </div>
                 </div>
-                </main>
             </div>
+
+
+            <div class="bg-black px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+                <div class="mx-auto mb-10 lg:max-w-xl sm:text-center">
+                    <p class="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-300 sm:text-4xl md:mx-auto">
+                    Team
+                    </p>
+                    <p class="text-base font-sans text-gray-600 md:text-lg">
+                    Meet the developers.
+                    </p>
+                </div>
+                <div class="grid gap-10 mx-auto sm:grid-cols-2 lg:grid-cols-2 lg:max-w-screen-lg">
+                    <div>
+                    <div class="relative pb-56 mb-4 rounded shadow lg:pb-64">
+                        <img class="absolute object-cover w-full h-full rounded" src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260" alt="Person" />
+                    </div>
+                    <div class="flex flex-col sm:text-center">
+                        <p class="text-white text-lg font-sans">Jackson</p>
+                        <p class="mb-5 text-md text-white font-sans">He is the goat</p>
+                        <div class="flex items-center space-x-3 sm:justify-center">
+                        </div>
+                    </div>
+                    </div>
+                    <div>
+                    <div class="relative pb-56 mb-4 rounded shadow lg:pb-64">
+                        <img class="absolute object-cover w-full h-full rounded" src="https://images.pexels.com/photos/2381069/pexels-photo-2381069.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260" alt="Person" />
+                    </div>
+                    <div class="flex flex-col sm:text-center">
+                        <p class="text-white text-lg font-sans">Reid</p>
+                        <p class="mb-5 text-md text-white font-sans">Also kind of a goat</p>
+                        <div class="flex items-center space-x-3 sm:justify-center">
+                        </div>
+                    </div>
+                    </div>
+                
+                </div>
             </div>
-            <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-            <img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src="https://thumbs.dreamstime.com/b/d-cg-rendering-gold-man-170348858.jpg" alt=""/>
-            </div>
-        </div>
+
+
 
         </div>
     );
